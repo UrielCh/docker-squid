d97b697617e6a22a540f7e709724cac6f0613d2e
urielCh <uriel.chemouni@gmail.com>
2020-02-25 16:52:19 +0200

    debug

--- README.md
+++ README.md
@@ -6,3 +6,17 @@ this squid Docker give access to all the IP of the host
 
 - PASSWORD: choose password
 - PORT: port to bind
+
+## publish
+
+```bash
+docker build -f alpine.Dockerfile -t urielch/squid .
+docker push urielch/squid:latest
+```
+
+## clean
+
+```bash
+docker kill proxy2; docker rm $(docker  ps -a -q)
+docker pull urielch/squid:latest
+```
--- entry.sh
+++ entry.sh
@@ -17,7 +17,7 @@ rm -f /etc/squid/squid_users /etc/squid/users.conf
 touch /etc/squid/squid_users
 for IP in $(ip a | grep inet\  | grep brd | grep -v docker | grep -E -o 'inet [0-9.]+{8,}' | cut -d\  -f2)
 do
- echo ADD IP $IP
+ echo ADD IP $IP:$PORT
  htpasswd -b /etc/squid/squid_users user$C $PASSWORD
  echo acl user_$C proxy_auth user$C >> /etc/squid/users.conf
  echo tcp_outgoing_address $IP user_$C >> /etc/squid/users.conf
@@ -28,9 +28,13 @@ then
  echo 'No network outgoint interface founed'
  exit
 fi
-squid -z -f /etc/squid/squid.conf
+
+# cat /etc/squid/squid.conf
+
+squid -N -z -f /etc/squid/squid.conf
 echo cache OK
 sleep 1
-squid -N -f /etc/squid/squid.conf
+squid -X -N -f /etc/squid/squid.conf
+
 # cat /etc/squid/squid.conf
 # find / | grep ncsa_auth
